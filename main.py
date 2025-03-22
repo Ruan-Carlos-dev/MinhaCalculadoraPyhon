f412fd17cc0ce97ce05c0441f93da37bddbebed5
Ruan-Carlos-dev <ruancarlosdejesus0@gmail.com>
2025-03-22 19:48:09 -0300

    Adição de mais funções para calculadora

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import os
+import  os
 
 #user:str =  input("Insira seu nome:")
 #esteja preparado {user}''')
 
-
-num1= input("insira um número:")
-num2= input("segundo número:") 
-
-def somar():
-  num3 = num1 + num2 
-  return num3
+def soma():
+  resultado = num1 + num2 
+  return resultado
 
 def subtrair():
-    num3 = num1 - num2
-    return num3
+    resultado = num1 - num2
+    return resultado
 
 def dividir():
     num3 = num1 / num2
@@ -27,27 +23,41 @@ def finalizar():
         os.system('cls')
     elif os.name == 'posix': 
         os.system('clear')
-
     print("Finalizado\n")
+def formatar_resultado(valor):
+    return f"Resultado: {valor:.2f}".rstrip("0.")
 
+def actions():
+    print("1-somar(+)", "2-subtrair(-) ", "3-dividir(/)", "4-multiplicar(*)", "0-sair", sep="\n")
+    action:str= (input("Escolha sua ação: "))
+    return action
 
-print("1-somar(+)", "2-subtrair(-) ", "3-dividir(/)", "4-multiplicar(*)", "0-sair", sep="\n")
-action:str =(input("Escolha sua ação"))
+def itens(repost,action,action_symblo,descricao):
+            return repost == action or repost == action_symblo or repost.lower == descricao
+def calculos(): 
+    escolha= actions()
+    if itens(escolha,"1","+","somar"):
+        num3= soma()
+        print(formatar_resultado(num3))
 
-if action == "1" or "+" or "somar": 
-    print("")
+    elif itens(escolha,"2","-","subtrair"):
+        num3= subtrair()
+        print(formatar_resultado(num3))
 
-elif action == "2" or "-" or "subtrair":
-    print("")
+    elif itens(escolha,"3","*","multiplicar"):
+        num3= multiplicar()
+        print(formatar_resultado(num3))
 
-elif action == "3" or "/" or "dividir": 
-    print ("")
+    elif itens(escolha,"4","/","dividir"):
+        num3= dividir()
+        print(formatar_resultado(num3))
 
-elif action == "4" or "*" or "multiplicar":
-    print("")
+    elif actions() == "0" or escolha == "sair": 
+        finalizar()
 
-elif action == "0" or "sair": 
-    finalizar()
-else:
-    print("Ação não reconhecida, tente novamente!")
-
+    else:
+        print("Ação não reconhecida, tente novamente!")
+    
+num1= float(input("insira um número:"))
+num2= float(input("segundo número:"))
+calculos() 
